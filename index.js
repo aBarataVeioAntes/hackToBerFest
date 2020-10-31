c2aa71c9943a125634b5047435589041d6b4b9be
Erlitonlucas <erlitonfalucas@gmail.com>
2020-10-31 18:11:53 -0300

    primeiro commit :)

---
+++ index.js
@@ -0,0 +1,7 @@
+function mudarCor() {
+    if(document.body.style.backgroundColor == "darkgray"){
+    document.body.style.backgroundColor = "yellow";
+    } else{
+        document.body.style.backgroundColor = "darkgray";
+    }
+}
